df282b32394247e041649c86db16beecd15aef12
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-08-29 11:06:50 -0300

    index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from './routes';
+
+// Renderiza o aplicativo
+ReactDOM.render(
+  React.createElement(Router),
+  document.getElementById('root')
+);
 
