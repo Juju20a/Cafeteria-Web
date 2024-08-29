8cbd2e283cb0b7e9e5718b121a36b7399b8f9758
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-08-29 11:13:54 -0300

    routes.jsx

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,1 +1,41 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
+// Crie o roteador com as rotas
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,  // Componente layout principal
+    children: [
+      {
+        path: '/',
+        element: <Home />,  // Página inicial
+      },
+      {
+        path: 'about',
+        element: <//Layood />,  // Página Sobre
+      },
+      {
+        path: 'contact',
+        element: <Contact />,  // Página de Contato
+      },
+      {
+        path: '*',
+        element: <NotFound />,  // Página não encontrada
+      },
+    ],
+  },
+]);
+
+// Componente Router
+function Router() {
+  return <RouterProvider router={router} />;
+}
+
+export default Router;
 
